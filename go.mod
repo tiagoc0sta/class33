73e39dde94bda50074aa32b94aa860ac0d7f53af
Tiago Costa <tiagodpv@gmail.com>
2024-04-05 19:31:33 -0400

    f

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tiagoc0sta/class33
+
+go 1.21.5
---
+++ wiki.go
@@ -0,0 +1,8 @@
+package main
+
+//for handling errors
+//processing html templates
+//logging messages
+//for building websites
+
+//for regular expression matching
